b05085534a1c211caaf8778eab54da0ce9dfb18c
AleksandrSosninSysAd <aleksa.xrom@gmail.com>
2026-08-19 18:55:17 +0500

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "oauth_token" { type = string, sensitive = true }
+variable "cloud_id" { type = string }
+variable "folder_id" { type = string }
+variable "ubuntu_image_id" { type = string }
